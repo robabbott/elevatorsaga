4bd5b62c5195698febcfba5eef7bb63d59bb120b
Rob Abbott <rabbott@modernclimate.com>
2021-01-19 16:05:23 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,19 +1,29 @@
 {
   init: function(elevators, floors) {
+    let waiting = [];
+
     for (let e = 0; e < elevators.length; e++) {
       elevators[e].on('idle', function () {
         this.goToFloor(0);
       }).on('floor_button_pressed', function (floorNum) {
-        this.goToFloor(floorNum);
+        if (this.loadFactor()) {
+          this.goToFloor(floorNum);
+        }
       });
 
       for (let f = 0; f < floors.length; f++) {
         const level = floors[f].level;
 
         floors[f].on('up_button_pressed down_button_pressed', function () {
-          if (!elevators[e].loadFactor) {
-            elevators[e].goToFloor(level);
+          if (!waiting.includes(level)) {
+            waiting.push(level);
           }
+
+          if (!elevators[e].loadFactor()) {
+            elevators[e].goToFloor(waiting[0]);
+            waiting.splice(0, 1);
+          }
+
         });
       }
     }
