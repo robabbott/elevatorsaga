9e467fb30812977453df3110f7b6116cf09fddb5
Rob Abbott <rabbott@modernclimate.com>
2021-01-19 18:24:05 -0600

    through challenge 12

--- init.js
+++ init.js
@@ -4,27 +4,38 @@
 
     for (let e = 0; e < elevators.length; e++) {
       elevators[e].on('idle', function () {
-        this.goToFloor(0);
+        getNextPerson(this);
       }).on('floor_button_pressed', function (floorNum) {
-        if (this.loadFactor()) {
-          this.goToFloor(floorNum);
+        const pressedFloors = this.getPressedFloors();
+        const nextFloor = pressedFloors.length ? pressedFloors[0] : floorNum;
+        this.goToFloor(nextFloor);
+      });
+    }
+
+    for (let f = 0; f < floors.length; f++) {
+      const level = floors[f].level;
+
+      floors[f].on('up_button_pressed down_button_pressed', function () {
+        if (!waiting.includes(level)) {
+          waiting.push(level);
+        }
+
+        // if (!elevators[e].loadFactor()) {
+        //   getNextPerson(elevators[e]);
+        // }
+
+        for (let e = 0; e < elevators.length; e++) {
+          // console.log(elevators[e].destinationQueue)
         }
       });
+    }
 
-      for (let f = 0; f < floors.length; f++) {
-        const level = floors[f].level;
-
-        floors[f].on('up_button_pressed down_button_pressed', function () {
-          if (!waiting.includes(level)) {
-            waiting.push(level);
-          }
-
-          if (!elevators[e].loadFactor()) {
-            elevators[e].goToFloor(waiting[0]);
-            waiting.splice(0, 1);
-          }
-
-        });
+    function getNextPerson(elevator) {
+      if (waiting.length) {
+        elevator.goToFloor(waiting[0]);
+        waiting.splice(0, 1);
+      } else {
+        elevator.goToFloor(0);
       }
     }
   },
