4fadaeb3a3013bc4ef94e5366bb9ae07bdf68e23
Rob Abbott <rabbott@modernclimate.com>
2021-01-19 19:43:52 -0600

    Update init.js

--- init.js
+++ init.js
@@ -6,24 +6,28 @@
       elevators[e].on('idle', function () {
         getNextPerson(this);
       }).on('floor_button_pressed', function (floorNum) {
-        if (this.loadFactor()) {
-          this.goToFloor(floorNum);
+        const pressedFloors = this.getPressedFloors();
+        const nextFloor = pressedFloors.length ? pressedFloors[0] : floorNum;
+        this.goToFloor(nextFloor);
+      });
+    }
+
+    for (let f = 0; f < floors.length; f++) {
+      const level = floors[f].level;
+
+      floors[f].on('up_button_pressed down_button_pressed', function () {
+        if (!waiting.includes(level)) {
+          waiting.push(level);
+        }
+
+        // if (!elevators[e].loadFactor()) {
+        //   getNextPerson(elevators[e]);
+        // }
+
+        for (let e = 0; e < elevators.length; e++) {
+          // console.log(elevators[e].destinationQueue)
         }
       });
-
-      for (let f = 0; f < floors.length; f++) {
-        const level = floors[f].level;
-
-        floors[f].on('up_button_pressed down_button_pressed', function () {
-          if (!waiting.includes(level)) {
-            waiting.push(level);
-          }
-
-          if (!elevators[e].loadFactor()) {
-            getNextPerson(elevators[e]);
-          }
-        });
-      }
     }
 
     function getNextPerson(elevator) {
