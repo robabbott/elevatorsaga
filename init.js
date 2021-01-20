303d3e6ad6fd2734f90a3211b500bacb5f83bc7e
Rob Abbott <rabbott@modernclimate.com>
2021-01-19 19:40:29 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,16 +1,24 @@
 {
-    init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+  init: function(elevators, floors) {
+    for (let e = 0; e < elevators.length; e++) {
+      elevators[e].on('idle', function () {
+        this.goToFloor(0);
+      }).on('floor_button_pressed', function (floorNum) {
+        this.goToFloor(floorNum);
+      });
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
+      for (let f = 0; f < floors.length; f++) {
+        const level = floors[f].level;
+
+        floors[f].on('up_button_pressed down_button_pressed', function () {
+          if (!elevators[e].loadFactor) {
+            elevators[e].goToFloor(level);
+          }
         });
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
+      }
     }
+  },
+  update: function(dt, elevators, floors) {
+    // We normally don't need to do anything here
+  }
 }
